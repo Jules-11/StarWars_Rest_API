a01523a568741267a88fd840203971189b4a07ed
Jules-11 <julien.galeuzzi@gmail.com>
2022-08-13 10:44:57 +0000

    StarWars API project

--- src/main.py
+++ src/main.py
@@ -30,14 +30,97 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_people():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    people = Character.query.all()
+    people_serialize = list(map(lambda x: x.serialize(), people))
 
-    return jsonify(response_body), 200
+    return jsonify( people_serialize), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_single_people(people_id):
+
+    single_people = Character.query.get(people_id)
+    single_people_serialize = serialize(single_people)
+
+    return jsonify( single_people_serialize), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    planet = Planet.query.all()
+    planet_serialize = list(map(lambda x: x.serialize(), planet))
+
+    return jsonify(planet_serialize), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_single_planet(planet_id):
+
+    single_planet = Planet.query.get(planet_id)
+    single_planet_serialize = serialize(single_planet)
+
+    return jsonify(single_planet_serialize), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    users = User.query.all()
+    users_serialize = list(map(lambda x: x.serialize(), users))
+
+    return jsonify(users_serialize), 200
+
+@app.route('/user/favorites/<int:user_id>', methods=['GET'])
+def get_user_favorites(user_id):
+
+    user_favorites = Favorite.query.filter_by(user_id=user_id)
+    user_favorites_serialize = list(map(lambda x: x.serialize(), user_favorites))
+    
+    return jsonify(user_favorites_serialize), 200
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def set_favorite_planet(planet_id):
+    request_body = request.get_json(force=True)
+    user_id = request_body["user_id"]
+
+    favorite_planet = Favorite(user_id=user_id, planet_id=planet_id)
+
+    db.session.add(favorite_planet)
+    db.session.commit()
+     
+@app.route('/favorite/people/<int:character_id>', methods=['POST'])
+def set_favorite_people(character_id):
+    request_body = request.get_json(force=True)
+    user_id = request_body["user_id"]
+
+    favorite_character = Favorite(user_id=user_id, character_id=character_id)
+
+    db.session.add(favorite_character)
+    db.session.commit()
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    request_body = request.get_json(force=True)
+    user_id = request_body["user_id"]
+
+    delete_favorite_planet = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id)
+
+    db.session.delete(delete_favorite_planet)
+    db.session.commit()
+     
+@app.route('/favorite/people/<int:character_id>', methods=['DELETE'])
+def delete_favorite_people(character_id):
+    request_body = request.get_json(force=True)
+    user_id = request_body["user_id"]
+
+    delete_favorite_character = Favorite.query.filter_by(user_id=user_id, character_id=character_id)
+
+    db.session.delete(delete_favorite_character)
+    db.session.commit()
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
